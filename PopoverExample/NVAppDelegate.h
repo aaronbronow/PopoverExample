67161b10a078ff3eeeed77bd6c93e16353bc1858
Aaron Bronow <aaron@aaronbronow.com>
2017-10-09 13:14:27 -0700

    pushing downloaded example

--- PopoverExample/NVAppDelegate.h
+++ PopoverExample/NVAppDelegate.h
@@ -8,8 +8,17 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface NVAppDelegate : NSObject <NSApplicationDelegate>
+@interface NVAppDelegate : NSObject <NSApplicationDelegate, NSPopoverDelegate> {
+    NSButton *popoverTriggerButton;
+    NSPopover *popover;
+    NSWindow *popoverWindow;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSButton *popoverTriggerButton;
+@property (assign) IBOutlet NSPopover *popover;
+@property (assign) IBOutlet NSWindow *popoverWindow;
+
+- (IBAction)togglePopover:(id)sender;
 
 @end
--- PopoverExample/NVAppDelegate.m
+++ PopoverExample/NVAppDelegate.m
@@ -11,15 +11,40 @@
 @implementation NVAppDelegate
 
 @synthesize window = _window;
-
-- (void)dealloc
-{
-    [super dealloc];
-}
+@synthesize popoverTriggerButton;
+@synthesize popover;
+@synthesize popoverWindow;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+}
+
+- (BOOL)buttonIsPressed
+{
+    return self.popoverTriggerButton.intValue == 1;
+}
+
+- (IBAction)togglePopover:(id)sender
+{
+    if (self.buttonIsPressed) {
+        [self.popover showRelativeToRect:[popoverTriggerButton bounds] ofView:popoverTriggerButton preferredEdge:NSMaxYEdge];
+    } else {
+        [self.popover close];
+    }
+}
+
+- (NSWindow *)detachableWindowForPopover:(NSPopover *)popover
+{
+    return self.popoverWindow;
+}
+
+- (void)dealloc
+{
+    [popoverTriggerButton release];
+    [popover release];
+    [popoverWindow release];
+    [super dealloc];
 }
 
 @end
